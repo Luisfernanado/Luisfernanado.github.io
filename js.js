7465fc76bc39f0a6fbf378ebbcf283f1302c156d
Luisfernanado <luisfernando12u@gmail.com>
2023-08-16 22:23:37 -0400

    Creando mi primer sitio web

---
+++ js.js
@@ -0,0 +1,51 @@
+/*let usuario = prompt("untruduce el numero");
+
+if(usuario % !2) {
+    alert("el numero es par ")
+} else {
+    alert("el numero es impar");
+}*/
+
+
+
+/* about me 
+Mi nombre es Luis fernando Santana soy Front-end web developer y me gusta la programación.
+                Actualmente estoy estudiando programación y me gustaría que vean mis avances en este Portafolio.
+</>
+
+
+
+// definicion de diseno
+A la hora de diseñar me gusta tratar
+ de hacer el diseño de una forma sensible y
+que no se muy tedioso para las personas cuando
+visiten el Portafolio web. En este Portafolio 
+web tendrá la oportunidad de ver mi último proque es trabajado.
+
+</>
+
+
+//proyecto
+ todos los Proyecto realisados
+//
+
+//havilidades
+css
+html
+javascript
+Boostrap
+
+
+// hobby 
+futbol
+. leer 
+. programar
+. compartir con mis companeros
+</>
+
+//Formulario de contacto //
+
+
+
+// fppter y redes sociales //
+*/
